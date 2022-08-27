df9c91138a64aa365782881a80ed85efb641188b
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-08-27 21:35:39 +0600

    Adding Card component to wrap the css classes.

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,17 +1,22 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault(); //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur. For example, this can be useful when: Clicking on a "Submit" button, prevent it from submitting a form.
     }
    return (
-    <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"/>
-        <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" />
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="number"/>
+            <button type="submit">Add User</button>
+        </form>
+    </Card>
    );
 };
 
