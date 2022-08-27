f6b93539e4081f0d9057322a0f6fbf92c9d976d1
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-08-28 05:56:31 +0600

    Adding validation based on User Input

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -5,12 +5,23 @@ import classes from './AddUser.module.css';
 import Button from "../Button/Button";
 
 const AddUser = (props) => {
-    const [entredUsername, setEnteredUsername] = useState('');
+    const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault(); //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur. For example, this can be useful when: Clicking on a "Submit" button, prevent it from submitting a form.
-        console.log(entredUsername, enteredAge);
+        
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        //+ is used to convert any string to number...
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
    const onChangeUsernameHandler = (event) => {
@@ -25,9 +36,9 @@ const AddUser = (props) => {
     <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" onChange={onChangeUsernameHandler} />
+            <input id="username" type="text" value={enteredUsername} onChange={onChangeUsernameHandler} />
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" onChange={onChangeAgeHandler}/>
+            <input id="age" type="number" value={enteredAge} onChange={onChangeAgeHandler}/>
             <Button type="submit">Add User</Button>
         </form>
     </Card>
