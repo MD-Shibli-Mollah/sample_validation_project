cb87483bfd954d506c34ecc3e08cd4da16ea43ce
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-08-27 22:42:09 +0600

    useState is implemented to get the USER'S Input Value

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Card from "../UI/Card";
 import classes from './AddUser.module.css';
 import Button from "../Button/Button";
 
 const AddUser = (props) => {
+    const [entredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault(); //The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur. For example, this can be useful when: Clicking on a "Submit" button, prevent it from submitting a form.
-    }
+        console.log(entredUsername, enteredAge);
+    };
+
+   const onChangeUsernameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+   };
+
+   const onChangeAgeHandler = (event) => {
+    setEnteredAge(event.target.value);
+   }
+    
    return (
     <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" onChange={onChangeUsernameHandler} />
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"/>
+            <input id="age" type="number" onChange={onChangeAgeHandler}/>
             <Button type="submit">Add User</Button>
         </form>
     </Card>
